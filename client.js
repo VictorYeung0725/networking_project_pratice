2d2a49928776a43a8fcc4bc37c69f19520c2fd90
Victor <96713542+VictorYeung0725@users.noreply.github.com>
2025-01-07 15:05:04 +0800

    first init for establishing a UDP trial

--- client.js
+++ client.js
@@ -1,3 +1,18 @@
-import net from 'node:net';
+const net = require('node:net');
+const readline = require('readline/promises');
 
-net.createConnection();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+//create TCP client
+const client = net.createConnection({ host: '127.0.0.1', port: 3008 }, () => {
+  console.log('Connected to the server');
+
+  const message = rl.question('Enter a message');
+});
+
+client.on('end', () => {
+  console.log('Connection was ended');
+});
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-import net from 'node:net';
+const net = require('node:net');
 
 const server = net.createServer();
 
+server.on('connection', (socket) => {
+  console.log('A new connection to the server');
+
+  socket.write('Welcome to the server!\n');
+  console.log(socket);
+
+  socket.on('data', (data) => {
+    console.log('Received data:', data.toString());
+  });
+});
+
 server.listen(3008, '127.0.0.1', () => {
-  console.log('Open server on', server.address);
+  console.log('Open server on', server.address());
 });
