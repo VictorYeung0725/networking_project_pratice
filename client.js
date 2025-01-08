abe9f1534f1a8ae5ae3564da9437072487e4196b
Victor <96713542+VictorYeung0725@users.noreply.github.com>
2025-01-08 14:58:30 +0800

    adding a clearline function which improve the ui of terminal

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
+const { rejects } = require('node:assert');
 const net = require('node:net');
+const { resolve } = require('node:path');
 const readline = require('readline/promises');
 
 const rl = readline.createInterface({
@@ -6,15 +8,28 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((resolve, rejects) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
 //create TCP client
 const socket = net.createConnection(
   { host: '127.0.0.1', port: 3008 },
   async () => {
     console.log('Connected to the server');
 
-    const message = await rl.question('Enter a message >');
+    const ask = async () => {
+      const message = await rl.question('Enter a message >');
+      //clear the current line that the cursor is in
+      await clearLine(0);
+      socket.write(message);
+    };
 
-    socket.write(message);
+    ask();
   }
 );
 
