ead248aca739dce7ef1b3a81a92882722cab19df
Victor <96713542+VictorYeung0725@users.noreply.github.com>
2025-01-10 10:17:26 +0800

    deploy server on aws and connect to it with ec2 public address

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 const net = require('node:net');
 const readline = require('readline/promises');
 
+const PORT = 4020;
+const HOST = '18.140.54.250';
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -25,47 +28,44 @@ const moveCursor = (dx, dy) => {
 };
 
 //create TCP client
-const socket = net.createConnection(
-  { host: '127.0.0.1', port: 3008 },
-  async () => {
-    console.log('Connected to the server');
+const socket = net.createConnection({ host: HOST, port: PORT }, async () => {
+  console.log('Connected to the server');
 
-    const ask = async () => {
-      const message = await rl.question('Enter a message >');
-      //move the cursor one line up
-      await moveCursor(0, -1);
-      //clear the cursor line that the cursor is in
-      await clearLine(0);
-      socket.write(`${id}-message-${message}`);
-    };
+  const ask = async () => {
+    const message = await rl.question('Enter a message >');
+    //move the cursor one line up
+    await moveCursor(0, -1);
+    //clear the cursor line that the cursor is in
+    await clearLine(0);
+    socket.write(`${id}-message-${message}`);
+  };
+
+  ask();
+
+  socket.on('data', async (data) => {
+    //log an empty line
+    console.log();
+
+    //move the cursor one line up
+    await moveCursor(0, -1);
+
+    //clear the current line that cursor just moved into
+    await clearLine(0);
+
+    if (data.toString('utf-8').substring(0, 2) === 'id') {
+      //when we are getting the id...
+      //everthing from the third character up until the end
+      id = data.toString('utf-8').substring(3);
+
+      console.log(`Your id is ${id}!\n`);
+    } else {
+      //when we are getting the message
+      console.log(data.toString('utf-8'));
+    }
 
     ask();
-
-    socket.on('data', async (data) => {
-      //log an empty line
-      console.log();
-
-      //move the cursor one line up
-      await moveCursor(0, -1);
-
-      //clear the current line that cursor just moved into
-      await clearLine(0);
-
-      if (data.toString('utf-8').substring(0, 2) === 'id') {
-        //when we are getting the id...
-        //everthing from the third character up until the end
-        id = data.toString('utf-8').substring(3);
-
-        console.log(`Your id is ${id}!\n`);
-      } else {
-        //when we are getting the message
-        console.log(data.toString('utf-8'));
-      }
-
-      ask();
-    });
-  }
-);
+  });
+});
 
 socket.on('end', () => {
   console.log('Connection was ended');
