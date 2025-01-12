bb47612ec9e464db6f5ff3b9cd20e46a0bba992b
Victor <96713542+VictorYeung0725@users.noreply.github.com>
2025-01-12 14:22:01 +0800

    complete a simple uploader

--- client.js
+++ client.js
@@ -1,1 +1,20 @@
-const net = require('');
+const net = require('net');
+const fs = require('node:fs/promises');
+
+let filehandle, fileStream;
+
+const socket = net.createConnection({ host: '::1', port: 5050 }, async () => {
+  const filePath = './text.txt';
+  filehandle = await fs.open(filePath, 'r');
+  fileStream = filehandle.createReadStream();
+
+  //Reading from the source file
+  fileStream.on('data', (data) => {
+    socket.write(data);
+  });
+
+  fileStream.on('end', () => {
+    console.log('The file was successfully uploaded!');
+    socket.end();
+  });
+});
--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 const net = require('net');
+const fs = require('node:fs/promises');
+
+const server = net.createServer(() => {
+  console.log('server created');
+});
+
+let filehandle, fileStream;
+
+server.on('connection', (socket) => {
+  console.log('New Connection!');
+
+  socket.on('data', async (data) => {
+    filehandle = await fs.open(`storage/text.txt`, 'w');
+    fileStream = filehandle.createWriteStream();
+
+    //Writing to our destination file
+    fileStream.write(data);
+  });
+
+  socket.on('end', () => {
+    console.log('Connection ended');
+    filehandle.close();
+  });
+});
+
+server.listen(5050, '::1', () => {
+  console.log('Uploader server opened on', server.address());
+});
